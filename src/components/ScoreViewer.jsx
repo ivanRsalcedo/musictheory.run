c0777a2184d25f65eaef8b2cb996f6367d35c89a
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2026-03-20 00:45:16 -0400

    Implement basic working alphatab

--- src/components/ScoreViewer.jsx
+++ src/components/ScoreViewer.jsx
@@ -1,5 +1,35 @@
-function ScoreViewer() {
-    return <div>test</div>
-}
+import { useEffect, useRef } from 'react'
+import * as alphaTab from '@coderline/alphatab'
 
-export default ScoreViewer
+export default function ScoreViewer() {
+
+    const containerRef = useRef(null)
+    const apiRef = useRef(null)
+
+    useEffect(() => {
+        const api = new alphaTab.AlphaTabApi(containerRef.current,{
+            file: '/musicxml/test.musicxml',
+            player: {
+                enablePlayer: true,
+                soundFont: '/soundfont/sonivox.sf2'
+            }
+        })
+
+        apiRef.current = api
+
+        return () => {
+            api.destroy()
+        }
+    }, [])
+
+    const handlePlay = () => {
+        apiRef.current?.playPause()
+    }
+
+    return (
+        <>
+            <button onClick={handlePlay}>test</button>
+            <div ref={containerRef}></div>
+        </>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { alphaTab } from '@coderline/alphatab-vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), alphaTab()],
+  optimizeDeps: {
+    exclude: ['@coderline/alphatab'],
+  },
 })
